4ec56a0c6f3d2f53d7326a697d89b80a58b460a2
Walter Shewmake <waltershewmake@gmail.com>
2023-02-20 00:12:26 -0700

    Create new-drone-setup.js

---
+++ new-drone-setup.js
@@ -0,0 +1,20 @@
+
+// for every app in the apps.json, check if it's folder exists. If not, clone it.
+
+const fs = require('fs');
+const { exec } = require('child_process');
+
+const apps = JSON.parse(fs.readFileSync('./apps.json', 'utf8'));
+
+for (const app of apps) {
+    if (!fs.existsSync(`./${app.name}`)) {
+        exec(`git clone https://github.com/shewmake-design/${app.name}.git`, (err, stdout, stderr) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+    
+        console.log(`Successfully cloned ${app.name}.`);
+        });
+    }
+    }
