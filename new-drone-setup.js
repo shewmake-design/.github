fb8eaa5721fda7efa410d6c054058db0b77aafe8
Walter Shewmake <waltershewmake@gmail.com>
2023-02-20 00:18:37 -0700

    Update new-drone-setup.js

--- new-drone-setup.js
+++ new-drone-setup.js
@@ -8,7 +8,7 @@ const apps = JSON.parse(fs.readFileSync('./apps.json', 'utf8'));
 
 for (const app of apps) {
     if (!fs.existsSync(`./${app.name}`)) {
-        exec(`git clone https://github.com/shewmake-design/${app.name}.git`, (err, stdout, stderr) => {
+        exec(`git clone https://github.com/shewmake-design/${app.name}.git ./sites/${app.name}`, (err, stdout, stderr) => {
         if (err) {
             console.error(err);
             return;
