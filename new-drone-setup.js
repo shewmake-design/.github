d967fe1e1aac5dc4ea0fb1ec0f12a580f4559c5a
Drone <waltershewmake@gmail.com>
2023-02-20 20:30:41 +0000

    fix

--- new-drone-setup.js
+++ new-drone-setup.js
@@ -15,11 +15,11 @@ for (const app of apps) {
             console.log(`Building ${app.name}...`)
             console.time(`[${app.name}] Build finished in`);
             execSync(`echo Installing dependencies &&` +
-				`cd /home/drone/.github/sites/${app.name} && pnpm install --force && ` +
+				`cd /home/drone/.github/sites/${app.name} && pnpm install --force --shamefully-hoist && ` +
 				`echo Building site &&` +
 				`pnpm run build && ` +
 				`echo Restarting server &&` +
-                `cd /home/drone/.github && pm2 delete apps.json --only ${app.name} && pm2 start apps.json --only ${app.name} && ` +
+                `cd /home/drone/.github && pm2 delete apps.json --only ${app.name} && pm2 start apps.json --only ${app.name} && pm2 save && ` +
                 `echo Successfully built ${app.name}`, {
                     stdio: 'inherit'
                 });
