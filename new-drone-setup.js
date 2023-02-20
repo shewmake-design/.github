7a8ac4192c53bb32aa63cad8699387eb39b43902
Drone <waltershewmake@gmail.com>
2023-02-20 07:45:35 +0000

    fix

--- new-drone-setup.js
+++ new-drone-setup.js
@@ -5,7 +5,6 @@ const { execSync } = require('child_process');
 const apps = JSON.parse(fs.readFileSync('./apps.json', 'utf8'));
 
 console.log(`Cloning ${apps.length} apps...`);
-console.time('Cloned all apps in');
 for (const app of apps) {
     if (!fs.existsSync(`./sites/${app.name}`)) {
         execSync(`git clone https://github.com/shewmake-design/${app.name}.git ./sites/${app.name}`, (err, stdout, stderr) => {
@@ -18,39 +17,34 @@ for (const app of apps) {
             
             console.log(`Building ${app.name}...`)
             console.time(`Built ${app.name} in`)
-            const proc = execSync(`cd ./sites/${app.name} && npm install && npm run build`, (err, stdout, stderr) => {
-                if (err) {
-                    console.error(err);
-                    return;
-                }
-                console.log(`Successfully built ${app.name}.`);
-                console.timeEnd(`Built ${app.name} in`)
-
-
-            proc.stdout.on('data', (data) => {
-                console.log(`[${app.name}] ${data}`);
-            })
-                
-            proc.stderr.on('data', (data) => {
-
-                console.error(`[${app.name}] ${data}`);
-            }
-            )
+            const proc = execSync(`echo Installing dependencies &&` +
+				`cd /home/drone/.github/sites/${app.name} && npm install --force && ` +
+				`echo Building site &&` +
+				`npm run build && ` +
+				`echo Restarting server &&` +
+                `cd /home/.github && pm2 delete apps.json --only ${app.name} && pm2 start apps.json --only ${app.name} && ` +
+                `echo Successfully built ${app.name}`, (err, stdout, stderr) => {
+                    if (err) {
+                        console.error(err);
+                        return;
+                    }
             });
+            
+		proc.stdout.on("data", (data) => {
+			console.log(`[${req.body.repository.name}] ${data}`);
+		});
+		proc.stderr.on("data", (data) => {
+			console.error(`[${req.body.repository.name}] ${data}`);
+		});
+		proc.on("close", (code) => {
+			console.log(`----------------------------------------`);
+			console.log(
+				`[${req.body.repository.name}] Process exited with code ${code}`
+			);
+			console.timeEnd(`[${req.body.repository.name}] Build finished in`);
+			console.log(`----------------------------------------`);
+        })
+            console.timeEnd(`Built ${app.name} in`)
         });
     }
 }
-console.timeEnd('Cloned all apps in');
-
-console.log("Starting apps...");
-console.time('Started all apps in');
-execSync('pm2 start apps.json', (err, stdout, stderr) => {
-    if (err) {
-        console.error(err);
-        return;
-    }
-    
-    console.log('Successfully started apps.');
-});
-console.timeEnd('Started all apps in');
-console.timeEnd('Process finished in')
