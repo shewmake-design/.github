5d9123815f1c2a2b76b626d1729ed745502bfd8f
Walter Shewmake <waltershewmake@gmail.com>
2023-02-25 00:51:02 +0000

    fix

--- new-drone-setup.js
+++ new-drone-setup.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const { execSync } = require("child_process");
 const SITE_DIR = "/home/drone/actions-runner/_work";
 
-const apps = JSON.parse(fs.readFileSync("./apps.json", "utf8")).map(
+const apps = JSON.parse(fs.readFileSync("./apps.json", "utf8")).filter(
 	(app) => !fs.existsSync(`${SITE_DIR}/${app.name}`)
 );
 
