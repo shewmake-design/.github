5d336a1fe2ec8e4dd563148b78bc352a85ee1c6e
Walter Shewmake <waltershewmake@gmail.com>
2023-03-14 20:10:10 +0000

    support for new website package

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,27 @@
 const SITE_DIR = "/home/drone/actions-runner/_work";
 
 module.exports = {
-	apps: require("./apps.json").map((app) => ({
-		name: app.name,
-		cwd: `${SITE_DIR}/${app.name}/${app.name}`,
-		script: "npm run start -- -p $PORT",
-		max_memory_restart: "100M",
-		env: {
-			PORT: app.port,
-		},
-	})),
+	apps: require("./apps.json").map((app) => {
+		if (app.apiKey) {
+			return {
+				name: app.name,
+				cwd: `/home/drone/sites/${app.name}`,
+				script: "npm run start -- -p $PORT",
+				max_memory_restart: "100M",
+				env: {
+					PORT: app.port,
+					API_KEY: app.apiKey,
+				},
+			};
+		}
+		return {
+			name: app.name,
+			cwd: `${SITE_DIR}/${app.name}/${app.name}`,
+			script: "npm run start -- -p $PORT",
+			max_memory_restart: "100M",
+			env: {
+				PORT: app.port,
+			},
+		};
+	}),
 };
