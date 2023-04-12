a7d4134c833da6f31889cc8b26a78a47b56c2852
Walter Shewmake <waltershewmake@gmail.com>
2023-04-12 15:47:41 +0000

    added pull

--- new-drone-setup.js
+++ new-drone-setup.js
@@ -28,7 +28,9 @@ for (const app of apps) {
 			app.apiKey
 				? `/home/drone/sites/${app.name}`
 				: `${SITE_DIR}/${app.name}/${app.name}`
-		}${os.hostname() === "sat-00" ? " && git checkout dev" : ""}`,
+		}${
+			os.hostname() === "sat-00" ? " && git checkout dev && git pull" : ""
+		}`,
 		{
 			stdio: "inherit",
 		}
